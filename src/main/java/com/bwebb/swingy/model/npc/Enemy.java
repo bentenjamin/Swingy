10c40ed1cad566b785d323193362f446abdc9fe4
Ben Webb <bentenjamin@gmail.com>
2020-09-20 21:17:12 +0200

    created enemy class

---
+++ src/main/java/com/bwebb/swingy/model/npc/Enemy.java
@@ -0,0 +1,34 @@
+package com.bwebb.swingy.model.npc;
+
+import static com.bwebb.swingy.model.helper.MathFunctions.rand_gaus;
+import static java.lang.Math.pow;
+
+public class Enemy {
+    private int attack, defense, health, baseHealth = 10, baseDefense = 5, baseAttack = 10;
+
+    /* general equation used:
+        a + b * (c / d)^e
+        a = base health
+        b = 'target' health
+        c = player level
+        d = level at which you will hit b
+        e = how big the difference between levels will be
+     */
+    public Enemy(int playerLevel) {
+        health = (int) (baseHealth + (300 * pow((int)(playerLevel/10), 2)));
+        defense = (int) (baseDefense + (300 * pow((int)(playerLevel/10), 2)));
+        attack = (int) (baseAttack + (300 * pow((int)(playerLevel/10), 2)));
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+}
