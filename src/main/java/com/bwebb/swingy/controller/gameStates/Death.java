53ef40711e920efdcc625765bcb18e95700b2eae
Ben Webb <bentenjamin@gmail.com>
2020-09-21 16:34:19 +0200

    fixed death gamestate

--- src/main/java/com/bwebb/swingy/controller/gameStates/Death.java
+++ src/main/java/com/bwebb/swingy/controller/gameStates/Death.java
@@ -2,11 +2,24 @@ package com.bwebb.swingy.controller.gameStates;
 
 import com.bwebb.swingy.controller.GameStateParent;
 
-import static com.bwebb.swingy.controller.GameController.display;
+import static com.bwebb.swingy.controller.GameController.*;
 
 public class Death extends GameStateParent {
     @Override
     public void printMe() {
         display.death();
+    }
+
+    @Override
+    public boolean evaluate(String userInput) {
+        return true;
+    }
+
+    @Override
+    public void execute(String userInput) {
+        if (commands.containsKey(userInput))
+            commands.get(userInput).run();
+        else
+            currentState = gameStates.exploring;
     }
 }
